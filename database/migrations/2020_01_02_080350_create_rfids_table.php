9e95ff876bef98813197abc92e3cf5c7f48c8b5a
mohammed omar <omarmd2311@gmail.com>
2020-01-03 13:40:08 +0530

    changed db design migrations models

---
+++ database/migrations/2020_01_02_080350_create_rfids_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRfidsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rfids', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('RFID');
+            $table->bigInteger('department_id');
+            $table->foreign('department_id')->references('id')->on('Departments');
+            $table->bigInteger('component_id');
+            $table->foreign('component_id')->references('id')->on('components');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rfids');
+    }
+}
